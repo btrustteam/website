b59cc1819fb7a8e17c89d1e3f22c73774395ef06
Oluwatobi Bamidele <teebams49@gmail.com>
2024-09-15 16:43:19 +0100

    feat: eligibility

---
+++ src/app/grants/eligibility.tsx
@@ -0,0 +1,19 @@
+import Paragraph from "@/components/Paragraph";
+
+export default function Eligibility() {
+  return (
+    <div className="flex bg-[#222] w-full py-[3.5rem] px-[6.5rem] flex-col gap-6">
+      <h3 className="font-poppins font-bold text-[1.75rem] text-active-white">
+        Grant Eligibility
+      </h3>
+      <Paragraph className="!text-[1.25rem] !leading-[190%] w-[57.3125rem]">
+        Btrust grants are open to developers across Africa, India, Latin
+        America, MENA, and Southeast Asia. Although these regions are the
+        primary focus, we may also consider developers from other areas in the
+        Global South on a case-by-case basis, contingent on their Proof of Work.
+        The priority is to ensure that talented developers from these diverse
+        backgrounds have the opportunity to contribute to the Bitcoin ecosystem.
+      </Paragraph>
+    </div>
+  );
+}
--- src/app/grants/mainGrant.tsx
+++ src/app/grants/mainGrant.tsx
@@ -1,11 +1,13 @@
 "use client";
 import ActivitiesNewsletterAndFooter from "@/components/ActivitiesNewsLetterFooter";
+import Eligibility from "./eligibility";
 import GrantsHero from "./hero";
 
 export default function MainGrant() {
   return (
     <div className="flex flex-col">
       <GrantsHero />
+      <Eligibility />
       <ActivitiesNewsletterAndFooter />
     </div>
   );
