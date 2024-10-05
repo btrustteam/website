6ca0cd9f78e7b4b87f6094c6f0393514c040625d
Zero-1729 <zero1729@protonmail.com>
2024-10-05 23:15:02 +0100

    update: outline & arrow button support url link

--- src/components/buttons/OutlineButton.tsx
+++ src/components/buttons/OutlineButton.tsx
@@ -1,16 +1,20 @@
 interface OutlineButtonProps {
   text: string;
   className?: string;
-  onClick: () => void;
+  onClick?: () => void;
+  url?: string;
 }
 
 export default function OutlineButton({
   text,
   onClick,
   className,
+  url,
 }: OutlineButtonProps) {
   return (
-    <button
+    url ? 
+      <a href={url} className={`flex py-6 lg:py-[1rem] px-[1.5rem] items-center justify-center rounded-lg border border-active-white font-poppins text-base font-semibold text-active-white ${className}`}>{text}</a>
+      : <button
       onClick={onClick}
       className={`flex py-6 lg:py-[1rem] px-[1.5rem] items-center justify-center rounded-lg border border-active-white font-poppins text-base font-semibold text-active-white ${className}`}
     >
--- src/components/buttons/buttonWithArrow.tsx
+++ src/components/buttons/buttonWithArrow.tsx
@@ -1,18 +1,34 @@
 import Image from "next/image";
 interface ArrowButtonProps {
   text: string;
-  onClick: () => void;
+  onClick?: () => void;
+  url?: string;
   noIcon?: boolean;
   className?: string;
 }
 export default function ButtonWithArrow({
   text,
   onClick,
+  url,
   noIcon,
   className,
 }: ArrowButtonProps) {
   return (
-    <button
+    url ? 
+      <a
+      className={`flex justify-center items-center px-[2rem] lg:px-[1.5rem] py-6 lg:py-[1rem] rounded-lg bg-active-white text-base font-poppins font-semibold text-black-6 gap-2 ${className}`}
+      href={url}>
+        {text}
+        {!noIcon && (
+          <Image
+            src={"/arrow_right.svg"}
+            alt="arrow right"
+            width={8}
+            height={14}
+          />
+        )}
+      </a>
+    : <button
       className={`flex justify-center items-center px-[2rem] lg:px-[1.5rem] py-6 lg:py-[1rem] rounded-lg bg-active-white text-base font-poppins font-semibold text-black-6 gap-2 ${className}`}
       onClick={onClick}
     >
