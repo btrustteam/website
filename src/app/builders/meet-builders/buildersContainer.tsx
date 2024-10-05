828bc38d373ce758efb01bf6e4049e5d598e6763
Zero-1729 <zero1729@protonmail.com>
2024-10-06 00:15:45 +0100

    fix: adjust gap in meet builders builder container

--- src/app/builders/meet-builders/buildersContainer.tsx
+++ src/app/builders/meet-builders/buildersContainer.tsx
@@ -15,7 +15,7 @@ export default function BuildersContainer({
         text={`${year} Btrust Builders`}
         className={`text-[1.5rem] leading-[normal]`}
       />
-      <div className="flex flex-wrap items-center lg:items-start justify-center lg:justify-between gap-6 lg:gap-8">
+      <div className="flex flex-wrap items-center lg:items-start justify-center lg:justify-between gap-6 lg:gap-2">
         {builders.map((builder, index) => (
           <BuilderCard key={index} {...builder} />
         ))}
