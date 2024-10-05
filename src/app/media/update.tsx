ae43e6e98ae1cd36c65a6297a5125cecf876e748
Zero-1729 <zero1729@protonmail.com>
2024-10-05 21:30:26 +0100

    fix: take out media posts middle gap in desktop

--- src/app/media/update.tsx
+++ src/app/media/update.tsx
@@ -3,7 +3,7 @@ import MediaCard, { Media } from "./mediaCard";
 export default function Update({ updates }: { updates: Media[] }) {
   return (
     <div className="w-full flex flex-col gap-6 lg:gap-8">
-      <div className="flex w-full flex-wrap gap-6 lg:gap-8 items-center lg:items-start justify-center lg:justify-between">
+      <div className="flex w-full flex-wrap gap-6 lg:gap-0 items-center lg:items-start justify-center lg:justify-between">
         {updates.map((update, index) => (
           <MediaCard {...update} key={index} />
         ))}
