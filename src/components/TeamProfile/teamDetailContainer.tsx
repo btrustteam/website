ff543a043d40cf372dcdee68e8afaa09a5a7f848
Oluwatobi Bamidele <teebams49@gmail.com>
2024-10-06 19:32:46 +0100

    fix: animate team profile detail

--- src/components/TeamProfile/teamDetailContainer.tsx
+++ src/components/TeamProfile/teamDetailContainer.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Image from "next/image";
 import TeamCard, { TeamCardProps } from "./teamCard";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 interface TeamProps {
   title: string;
@@ -13,18 +16,38 @@ export default function TeamDetailsContainer({
   teamMembers,
   isBoard,
 }: TeamProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  const toggleOpen = () => setIsOpen(!isOpen);
   return (
     <div className="flex flex-col gap-6">
       <div className="flex flex-col gap-2">
-        <div className="flex w-full justify-between items-center gap-6">
+        <div
+          className="flex w-full justify-between items-center gap-6 cursor-pointer"
+          onClick={toggleOpen}
+        >
           <h3 className="text-active-white font-poppins text-[1.5rem] font-bold ">
             {title}
           </h3>
-          <Image src={"/caret.svg"} alt="caret" width={24} height={24} />
+          <motion.div
+            animate={{ rotate: isOpen ? 180 : 0 }}
+            transition={{ duration: 0.3 }}
+            className="flex justify-center items-center"
+          >
+            <Image src={"/caret.svg"} alt="caret" width={24} height={24} />
+          </motion.div>
         </div>
-        <p className="text-black-2 font-poppins text-[1.125rem] leading-[177.778%] w-w-full lg:w-[41.75rem]">
+        <motion.p
+          initial={{ height: 0, opacity: 0 }}
+          animate={{
+            height: isOpen ? "auto" : 0,
+            opacity: isOpen ? 1 : 0,
+          }}
+          transition={{ duration: 0.5 }}
+          className="text-black-2 font-poppins text-[1.125rem] leading-[177.778%] w-w-full lg:w-[41.75rem]"
+        >
           {brief}
-        </p>
+        </motion.p>
       </div>
       <div
         className={`flex flex-col items-center justify-center lg:flex-row gap-6 ${
