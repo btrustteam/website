ef8fbf2ce87738355ad97ff59ca785f292aaebcf
Oluwatobi Bamidele <teebams49@gmail.com>
2024-10-13 15:12:38 +0100

    feat: metadata for builders page

--- src/app/builders/page.tsx
+++ src/app/builders/page.tsx
@@ -1,7 +1,24 @@
 "use client";
 import ActivitiesNewsletter from "@/components/ActivitiesNewsLetterFooter";
+import { Metadata } from "next";
 import BuildersHero from "./buildersHero";
 import ProcessToJoin from "./process";
+
+export const metadata: Metadata = {
+  title: "Btrust Builders",
+  description: "Empowering Open-Source Bitcoin Builders in Africa",
+  openGraph: {
+    type: "website",
+    title: "Btrust Builders",
+    description: "Empowering Open-Source Bitcoin Builders in Africa",
+    url: "https://www.btrust.tech/builders",
+    images: [
+      {
+        url: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/Rectangle%2023-iiQiySKc69hB8Hey4eke2g4XzfnbFB.png",
+      },
+    ],
+  },
+};
 
 export default function Builders() {
   return (
