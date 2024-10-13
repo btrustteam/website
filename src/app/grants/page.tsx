65866752cd6f629df80738d4149d5a215dca6a7d
Oluwatobi Bamidele <teebams49@gmail.com>
2024-10-13 14:57:25 +0100

    feat: metadata added for grant page

--- src/app/grants/page.tsx
+++ src/app/grants/page.tsx
@@ -1,7 +1,25 @@
 "use client";
+import { Metadata } from "next";
 import { useState } from "react";
 import MainGrant from "./mainGrant";
 import Recipients from "./recipients";
+
+export const metadata: Metadata = {
+  title: "Grants",
+  description: "Get funding to work on Free and Open-Source Bitcoin projects",
+  openGraph: {
+    type: "website",
+    title: "Grants",
+    description: "Get funding to work on Free and Open-Source Bitcoin projects",
+    url: "https://www.btrust.tech/grants",
+    images: [
+      {
+        url: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/Rectangle%2018-brrzAlRT8KT2clTDRTKjrimxmY3RTC.png",
+      },
+    ],
+  },
+};
+
 export default function Grants() {
   const [currentPage, setCurrentPage] = useState<string>("");
   return (
