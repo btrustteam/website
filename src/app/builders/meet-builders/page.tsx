d80fbc484b7f558830bdfc00e9f919ae7ab5b973
Oluwatobi Bamidele <teebams49@gmail.com>
2024-10-13 15:19:12 +0100

    feat: metadata for meet builders page

--- src/app/builders/meet-builders/page.tsx
+++ src/app/builders/meet-builders/page.tsx
@@ -1,8 +1,25 @@
 "use client";
 import ActivitiesNewsletter from "@/components/ActivitiesNewsLetterFooter";
+import { Metadata } from "next";
 import CurrentBuilders from "./currentBuilder";
 import Hero from "./hero";
 import PotentialBuilder from "./potential";
+
+export const metadata: Metadata = {
+  title: "Meet Btrust builders",
+  description: "Meet The Builders",
+  openGraph: {
+    type: "website",
+    title: "Meet Btrust builders",
+    description: "Meet The Builders",
+    url: "https://www.btrust.tech/builders/meet-builders",
+    images: [
+      {
+        url: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/Screenshot%202023-12-28%20at%2008.44%201-ni992ux3ckchiqqnxdCdiuWhvWWqH8.png",
+      },
+    ],
+  },
+};
 
 export default function MeetBuilders() {
   return (
