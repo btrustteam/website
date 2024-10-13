0f3a56b88c522c4bd4687ef76513d0d93a4557ad
Oluwatobi Bamidele <teebams49@gmail.com>
2024-10-13 15:08:53 +0100

    feat: metadata for contact us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,24 @@
 "use client";
 import ActivitiesNewsletter from "@/components/ActivitiesNewsLetterFooter";
+import { Metadata } from "next";
 import Enquiry from "./enquiry";
 import Hero from "./hero";
+
+export const metadata: Metadata = {
+  title: "Contact Us",
+  description: "We’d Love to Hear from You",
+  openGraph: {
+    type: "website",
+    title: "Contact Us",
+    description: "We’d Love to Hear from You",
+    url: "https://www.btrust.tech/contact",
+    images: [
+      {
+        url: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/Rectangle%2041%20(4)-mJpb1uDxiZZWzI5TSKOMminFDJJcJF.png",
+      },
+    ],
+  },
+};
 
 export default function Contact() {
   return (
