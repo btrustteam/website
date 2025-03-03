6a3a6615d2fe4633c587511952d5a1473081168b
Oluwatobi Bamidele <teebams49@gmail.com>
2025-03-04 00:19:26 +0100

    chore: revamp grant main page

--- src/app/grants/mainGrant.tsx
+++ src/app/grants/mainGrant.tsx
@@ -1,5 +1,7 @@
 "use client";
 import ActivitiesNewsletter from "@/components/ActivitiesNewsLetterFooter";
+import AllGrants from "./allGrantContainer";
+import BtrustGrant from "./btrustGrant";
 import Cohort from "./cohort";
 import CohortStructure from "./cohortStructure";
 import Eligibility from "./eligibility";
@@ -15,11 +17,13 @@ export default function MainGrant({
   return (
     <div className="flex flex-col">
       <GrantsHero />
-      <Eligibility />
-      <StartGrant setPage={setPage} />
-      <Perks />
-      <Cohort setPage={setPage} />
-      <CohortStructure />
+      <BtrustGrant />
+      {/* <Eligibility /> */}
+      {/* <StartGrant setPage={setPage} /> */}
+      <AllGrants />
+      {/* <Perks /> */}
+      {/* <Cohort setPage={setPage} /> */}
+      {/* <CohortStructure /> */}
       <ActivitiesNewsletter />
     </div>
   );
