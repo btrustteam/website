c49fa827655339adc856e54f47c402c35b748dd4
Oluwatobi Bamidele <teebams49@gmail.com>
2025-03-13 15:47:53 +0100

    chore: add hero image to all grant page

--- src/app/grants/developer/overviewHero.tsx
+++ src/app/grants/developer/overviewHero.tsx
@@ -1,3 +1,4 @@
+import DynamicImage from "@/components/DynamicImage";
 import LittleHeading from "@/components/LittleHeading";
 import SubHeading from "@/components/SubHeading";
 import Image from "next/image";
@@ -8,7 +9,7 @@ export default function OverviewHero({
   setPage: (page: string) => void;
 }) {
   return (
-    <div className="w-full flex flex-col gap-6 lg:gap-20 my-[2rem] lg:my-[3rem]">
+    <div className="w-full flex flex-col gap-6 lg:gap-20 mt-[2rem] lg:mt-[3rem]">
       <div className="px-[2rem] lg:px-[6.5rem] py-0">
         <div
           className="flex items-center gap-2 cursor-pointer z-50"
@@ -28,6 +29,13 @@ export default function OverviewHero({
         <SubHeading
           text="Get funding to work on Free and Open-Source Bitcoin projects"
           className="w-full lg:w-[58.375rem]"
+        />
+      </div>
+      <div className="flex justify-center items-center bg-red-300">
+        <DynamicImage
+          src="https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/BTrust%20Gathering%20Day%202%20-103-UDt5Jos2fBnxEZptEr3WdJEew6d635.JPG"
+          alt=""
+          className="w-full h-[18.75rem] lg:h-[46.1875rem] object-cover"
         />
       </div>
     </div>
