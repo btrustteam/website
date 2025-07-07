a17e76b52f6c4ba81631153b3e106e55be8d1bb8
Kelvin Isievwore <kelvinator4leo@gmail.com>
2025-07-07 18:45:24 +0100

    update bitdevs page

--- src/app/bitdevs/bitDevsCard.tsx
+++ src/app/bitdevs/bitDevsCard.tsx
@@ -1,68 +1,56 @@
+import Image from "next/image";
 import OutlineButton from "@/components/buttons/OutlineButton";
-import DynamicImage from "@/components/DynamicImage";
 import Paragraph from "@/components/Paragraph";
 import SectionHeader from "@/components/sectionHeader";
 
-export default function BitDevsCard({
-  imageSrc,
-  details,
-  website,
-  xlink,
-  title,
-}: {
-  imageSrc: string;
-  details: string[];
-  website: string;
-  xlink: string;
-  title: string;
-}) {
-  return (
-    <div
-      className={`flex flex-col gap-6 lg:flex-row justify-between mb-12 lg:mb-24`}
-    >
-      <div className="lg:hidden flex w-full">
-        <SectionHeader
-          text={title}
-          className="!text-[1.625rem] !leading-[normal] !uppercase"
-        />
-      </div>
-      <DynamicImage
-        src={imageSrc}
-        className="w-full h-[18.75rem] lg:h-full lg:w-[35.125rem] rounded-lg object-cover"
-        alt="participants"
-      />
-      <div className="w-full lg:w-[37.125rem] flex flex-col gap-10">
-        <div className="w-full flex flex-col gap-4">
-          <SectionHeader
-            text={title}
-            className="!text-[2.5rem] !leading-[140%] !uppercase hidden lg:flex"
-          />
-          <div className="flex flex-col w-full gap-6">
-            {details.map((text, index) => (
-              <Paragraph key={index} className="text-black-2">
-                {text}
-              </Paragraph>
-            ))}
-          </div>
-        </div>
+interface BitDevsCardProps {
+    imageSrc: string;
+    details: string;
+    website: string;
+    xlink: string;
+    title: string;
+}
 
-        <div className="flex flex-col gap-4">
-          <OutlineButton
-            text="Visit Site"
-            className="w-full lg:w-auto"
-            url={website}
-            iconUrl="/link.svg"
-            newTab={true}
-          />
-          <OutlineButton
-            text="Follow X Page"
-            className="w-full lg:w-auto"
-            url={xlink}
-            iconUrl="/link.svg"
-            newTab={true}
-          />
+export default function BitDevsCard({
+    imageSrc,
+    details,
+    website,
+    xlink,
+    title,
+}: BitDevsCardProps) {
+    return (
+        <div className="flex flex-col w-full max-w-[24.6875rem] lg:w-[24.6875rem] p-[1.25rem] gap-[1rem] rounded-lg backdrop-blur-[2px] border border-card-border">
+            <Image
+                src={imageSrc}
+                width={0}
+                height={0}
+                sizes={"100vw"}
+                className="h-[14.25rem] w-full rounded-lg object-cover"
+                alt="blog cover"
+            />
+            <div className="flex flex-col gap-[0.75rem]">
+                <SectionHeader
+                    text={title}
+                    className="!leading-[155.556%] lg:!leading-[175%] lg:!text-[1.25rem] !text-[1.125rem]"
+                />
+                <Paragraph className="">{details}</Paragraph>
+            </div>
+            <div className="flex flex-col gap-4">
+                <OutlineButton
+                    text="Visit Site"
+                    className="w-full lg:w-auto"
+                    url={website}
+                    iconUrl="/link.svg"
+                    newTab={true}
+                />
+                <OutlineButton
+                    text="Follow X Page"
+                    className="w-full lg:w-auto"
+                    url={xlink}
+                    iconUrl="/link.svg"
+                    newTab={true}
+                />
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/app/bitdevs/bitDevsContainer.tsx
+++ src/app/bitdevs/bitDevsContainer.tsx
@@ -4,138 +4,99 @@ export default function AllBitDevs() {
     const bitDevs = [
         {
             title: "BitDevs Abuja",
-            details: [
-                "BitDevsAbuja is a community in Abuja, Nigeria, for those interested in discussing and participating in the research and development of Bitcoin and related protocols and software.",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/abuja-BqbNZPBt6H8NI3mgOgReZGA50sZPSE.jpg",
+            details: "BitDevsAbuja is a community in Abuja, Nigeria, for those interested in discussing and participating in the research and development of Bitcoin and related protocols and software.",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/abuja-BqbNZPBt6H8NI3mgOgReZGA50sZPSE.jpg",
             website: "https://bitdevsabuja.org/",
             xlink: "https://x.com/BitDevsAbuja",
         },
         {
             title: "BitDevs Accra",
-            details: [
-                "BitDevsAccra is a community in Ghana for those interested in discussing and participating in the research and development of Bitcoin and related pr.",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/accra-BcbkbuABNXJAqnAuGPn2i6lBCRQrM6.jpeg",
+            details: "BitDevsAccra is a community in Ghana for those interested in discussing and participating in the research and development of Bitcoin and related pr.",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/accra-BcbkbuABNXJAqnAuGPn2i6lBCRQrM6.jpeg",
             website: "https://bitdevsaccra.org/",
             xlink: "https://x.com/bitdevsAccra",
         },
         {
           title: "BitDevs Cotonou",
-          details: [
-              "BitDevs Cotonou is a community in Cotonou, Benin, for those interested in discussing and participating in the research and development of Bitcoin and related protocols in Cotonou.",
-          ],
-          imageSrc:
-              "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/cotonou-DyjxNcNb5SxXbnykZWbb3HmoZZHvlV.jpg",
+          details: "BitDevs Cotonou is a community in Cotonou, Benin, for those interested in discussing and participating in the research and development of Bitcoin and related protocols in Cotonou.",
+          imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/cotonou-DyjxNcNb5SxXbnykZWbb3HmoZZHvlV.jpg",
           website: "https://bitdevscotonou.org/",
           xlink: "https://x.com/BitdevsCotonou",
       },
       {
             title: "BitDevs Duoala",
-            details: [
-                "Douala Bitcoin Development community",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/douala-1llcW9xC9rzx3fPbHn0Jl2JWpoTF11.jpg",
+            details: "Douala Bitcoin Development community",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/douala-1llcW9xC9rzx3fPbHn0Jl2JWpoTF11.jpg",
             website: "https://bitdevsdla.org/",
             xlink: "https://x.com/BitDevsDLA",
         },
         {
             title: "BitDevs Gitega",
-            details: [
-                "BitDevs Gitega is a community of Bitcoin developers, educators, builders, and technically curious enthusiasts based in Gitega.",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/gitega-xLKta0cDBaz1IHV7gWy9my6cTboO1c.jpg",
+            details: "BitDevs Gitega is a community of Bitcoin developers, educators, builders, and technically curious enthusiasts based in Gitega.",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/gitega-xLKta0cDBaz1IHV7gWy9my6cTboO1c.jpg",
             website: "https://bitdevsgtga.org/",
             xlink: "https://x.com/bitdevsgtga",
         },
         {
             title: "BitDevs Goma",
-            details: [
-                "BitDevsGoma est un meetup (rencontre) consacré à la recherche et au développement de Bitcoin.",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/goma-XmBwmBT6C57d6i5SP3sfd7EmZdhde2.jpg",
+            details: "BitDevsGoma est un meetup (rencontre) consacré à la recherche et au développement de Bitcoin.",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/goma-XmBwmBT6C57d6i5SP3sfd7EmZdhde2.jpg",
             website: "https://bitdevsgoma.org/",
             xlink: "https://x.com/BitdevsGoma",
         },
         {
             title: "BitDevs Joburg",
-            details: [
-                "A Johannesburg BitDevs community meet-up with socratic style seminars diving into new developments in #Bitcoin",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/joburg-nYFPoJObQ7JRMlyV1ZSFJR9wrhDVzL.jpg",
+            details: "A Johannesburg BitDevs community meet-up with socratic style seminars diving into new developments in #Bitcoin",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/joburg-nYFPoJObQ7JRMlyV1ZSFJR9wrhDVzL.jpg",
             website: "https://bitdevs.joburg",
             xlink: "https://x.com/BitDevsJHB",
         },
         {
             title: "BitDevs Kaduna",
-            details: [
-                "BitDevs Kaduna is a community where Bitcoin developers and enthusiasts gather to engage in technical discussions and collaborate on Bitcoin development #bitcoin",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/kaduna-m3LeNOlSiOOBoQwLoj8fjL0M38OgTw.jpeg",
+            details: "BitDevs Kaduna is a community where Bitcoin developers and enthusiasts gather to engage in technical discussions and collaborate on Bitcoin development #bitcoin",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/kaduna-m3LeNOlSiOOBoQwLoj8fjL0M38OgTw.jpeg",
             website: "https://bitdevskaduna.org/",
             xlink: "https://x.com/BitDevsKaduna",
         },
         {
             title: "BitDevs Kampala",
-            details: [
-                "Kampala Bitcoin Developers Community.",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/kampala-nKItRU1arxVZPaGcYhLEiFR4AHgqM6.jpg",
+            details: "Kampala Bitcoin Developers Community.",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/kampala-nKItRU1arxVZPaGcYhLEiFR4AHgqM6.jpg",
             website: "https://bitdevskla.org/",
             xlink: "https://x.com/BitDevsKLA",
         },
         {
             title: "BitDevs Kano",
-            details: [
-                "A place for free and open #Bitcoin education in #Kano. Join the community and #LearnBitcoinTogether!",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/abuja-BqbNZPBt6H8NI3mgOgReZGA50sZPSE.jpg",
+            details: "A place for free and open #Bitcoin education in #Kano. Join the community and #LearnBitcoinTogether!",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/abuja-BqbNZPBt6H8NI3mgOgReZGA50sZPSE.jpg",
             website: "https://bitdevskano.org/",
             xlink: "https://x.com/bitdevskano",
         },
         {
             title: "BitDevs Lagos",
-            details: [
-                "Bitdev Lagos is a community of Bitcoin developers working together to foster innovation and collaboration in Nigeria. Join us. #Bitcoin #Nigeria.",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/lagos-8pPpeMnN9TixGje8QaRWGrJpm4A77h.jpg",
+            details: "Bitdev Lagos is a community of Bitcoin developers working together to foster innovation and collaboration in Nigeria. Join us. #Bitcoin #Nigeria.",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/lagos-8pPpeMnN9TixGje8QaRWGrJpm4A77h.jpg",
             website: "https://bitdevslagos.org/",
             xlink: "https://x.com/BitDevsLagos",
         },
         {
             title: "BitDevs Nairobi",
-            details: [
-                "BitDevsNBO is a meetup devoted to the research and development of Bitcoin.",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/nairobi-qwGoJis4txjl9Ts4pyWr7zJhSiwnKI.png",
+            details: "BitDevsNBO is a meetup devoted to the research and development of Bitcoin.",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/nairobi-qwGoJis4txjl9Ts4pyWr7zJhSiwnKI.png",
             website: "https://bitdevsnbo.org/",
             xlink: "https://x.com/BitDevsNBO",
         },
         {
             title: "BitDevs Uyo",
-            details: [
-                "A community of Bitcoin developers in Uyo. Learning, building, and exploring Bitcoin together. #BitDevsUyo #Bitcoin",
-            ],
-            imageSrc:
-                "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/uyo-4Vq7l1b59oWJWEt93Nl25oPWRqXbp8.png",
+            details: "A community of Bitcoin developers in Uyo. Learning, building, and exploring Bitcoin together. #BitDevsUyo #Bitcoin",
+            imageSrc: "https://8aqkfzpsopxwkjhh.public.blob.vercel-storage.com/uyo-4Vq7l1b59oWJWEt93Nl25oPWRqXbp8.png",
             website: "https://bitdevsuyo.org/",
             xlink: "https://x.com/BitDevsUyo",
         },
         
     ];
     return (
-        <div className="pt-[4.5rem] lg:pt-[8rem] lg:pb-0 px-6 lg:px-[6.5rem]">
+        <div className="flex flex-wrap pt-[4.5rem] lg:pt-[8rem] lg:pb-0 px-6 lg:px-[6.5rem] gap-6 lg:gap-8">
             {bitDevs.map((bitDev, index) => (
                 <BitDevsCard key={index} {...bitDev} />
             ))}
